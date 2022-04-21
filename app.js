6150f82d3b5eef4efc4cbf8eb7ddb54d094ddf42
unknown <andrew.joe.jones@gmail.com>
2022-04-20 23:14:16 -0700

    basic backend

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+comments = [];
+
+app.get('/api/comments', (req, res) => {
+  res.send(comments)
+})
+
+app.post('/api/comment', (req, res) => {
+  comments.push(req.body);
+  res.send("good job!");
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
