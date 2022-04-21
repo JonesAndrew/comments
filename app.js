b4195ef71a3304c275b73e2278738a656aa2045d
unknown <andrew.joe.jones@gmail.com>
2022-04-21 10:51:27 -0700

    V2 mvp, needs some cleanup and testing

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const sqlite3 = require('sqlite3');
 const express = require('express');
+const ws = require('ws');
 const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
 
 const uuid  = require('uuid');
 
+const wsServer = new ws.Server({ noServer: true });
+wsServer.on('connection', socket => {
+  socket.on('message', message => console.log(message));
+});
+
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/comments', (req, res) => {
-  db.all(`SELECT comments.comment_id, comments.name, comment, count(upvotes.name) AS upvotes
+  db.all(`SELECT comments.comment_id, comments.timestamp, comments.name, parent_id, comment, count(upvotes.name) AS upvotes
           FROM comments LEFT JOIN upvotes ON comments.comment_id == upvotes.comment_id GROUP BY comments.comment_id`,
           [], (err, rows) => {
 
@@ -26,17 +32,22 @@ app.get('/api/comments', (req, res) => {
 app.post('/api/comment', (req, res) => {
   let comment = req.body;
 
-  db.get(`INSERT INTO comments(comment_id, name, comment) VALUES (?, ?, ?)`, [uuid.v4(), comment.name, comment.comment], (err, row) => {
+  db.get(`INSERT INTO comments(comment_id, name, comment, parent_id) VALUES (?, ?, ?, ?)`, [uuid.v4(), comment.name, comment.comment, comment.parent_id], (err, row) => {
     if (err) {
       res.status(400);
       return;
     }
 
     res.send("good job!");
+
+    wsServer.clients.forEach(function each(client) {
+       client.send('refresh');
+    });
   });
 })
 
 app.post('/api/upvote', (req, res) => {
+  console.log(req.body)
   db.get(`INSERT INTO upvotes(comment_id, name) VALUES (?, ?)`, [req.body.comment_id, req.body.name], (err, row) => {
     if (err) {
       res.status(400);
@@ -44,12 +55,22 @@ app.post('/api/upvote', (req, res) => {
     }
 
     res.send("good job!");
+  
+    wsServer.clients.forEach(function each(client) {
+       client.send('refresh');
+    });
   });
 })
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+server.on('upgrade', (request, socket, head) => {
+  wsServer.handleUpgrade(request, socket, head, socket => {
+    wsServer.emit('connection', socket, request);
+  });
+});
 
 const db = new sqlite3.Database('./comments.db', (err) => {
   if (err) {
@@ -57,12 +78,12 @@ const db = new sqlite3.Database('./comments.db', (err) => {
     return;
   }
 
-  db.run('CREATE TABLE comments(comment_id TEXT PRIMARY KEY, name text, comment text)', (err) => {
+  db.run('CREATE TABLE comments(comment_id TEXT PRIMARY KEY, name text, comment text, parent_id TEXT, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)', (err) => {
     if (err)
       console.log('Hopefully the tables already exist', err);
   });
 
-  db.run('CREATE TABLE upvotes(comment_id TEXT, name text, PRIMARY KEY(comment_id, name))', (err) => {
+  db.run('CREATE TABLE upvotes(comment_id TEXT, name text, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(comment_id, name))', (err) => {
     if (err)
       console.log('Hopefully the tables already exist', err);
   });
---
+++ public/comments.js
@@ -0,0 +1,208 @@
+function urlEncode(details) {
+  //https://stackoverflow.com/questions/35325370/how-do-i-post-a-x-www-form-urlencoded-request-using-fetch
+  let formBody = [];
+  for (let property in details) {
+    let encodedKey = encodeURIComponent(property);
+    let encodedValue = encodeURIComponent(details[property]);
+    formBody.push(encodedKey + "=" + encodedValue);
+  }
+  return formBody.join("&");
+}
+
+class Comment extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {commenting: false }
+
+    this.upvote = this.upvote.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.reply = this.reply.bind(this);
+  }
+
+  reply() {
+    this.setState({ commenting: !this.state.commenting });
+  }
+
+  upvote() {
+    fetch('/api/upvote', {
+
+      method: 'POST', 
+      body: urlEncode({comment_id: this.props.id, name: CURRRENT_NAME}),
+      headers: { 'content-type': 'application/x-www-form-urlencoded' },
+
+    });
+  }
+
+  onSubmit() {
+    this.setState({ commenting: false });
+  }
+
+  render() {
+
+    return (
+        <div class="comment one-line">
+            <img alt="140x140" data-src="holder.js/140x140" class="rounded-circle profile" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgdmlld0JveD0iMCAwIDE0MCAxNDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzE0MHgxNDAKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xODA0YTZlMTUzYyB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE4MDRhNmUxNTNjIj48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjQ0LjA0Njg3NSIgeT0iNzQuNSI+MTQweDE0MDwvdGV4dD48L2c+PC9nPjwvc3ZnPg==" data-holder-rendered="true" />
+          <div class>
+            <div class="one-line">
+              <p class="font-weight-bold">{this.props.name}</p>
+              <p>·</p>
+              <p class="text-secondary">{ this.props.timestamp.fromNow() }</p>
+            </div>
+            <div>
+              <p>{this.props.comment}</p>
+            </div>
+            <div class="one-line">
+              <button onClick={this.upvote} class="font-weight-bold sneaky-btn">▲ Upvote</button>
+              <p> {this.props.upvotes} </p>
+              <button onClick={this.reply} class="font-weight-bold sneaky-btn">Reply</button>
+            </div>
+            { this.state.commenting && <CommentBox onSubmit={this.onSubmit} parentId={this.props.id} /> }
+            { 
+              this.props.children && this.props.children.map((c) => 
+                <Comment key={c.comment_id} id={c.comment_id} timestamp={c.timestamp} name={c.name} comment={c.comment} upvotes={c.upvotes} children={c.children} /> 
+              ) 
+            } 
+          </div>
+        </div>
+    );
+  }
+}
+
+class CommentBox extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+
+    this.state = {text: ""};
+  }
+
+  onSubmit() {
+    fetch('/api/comment', {
+
+      method: 'POST', 
+      body: urlEncode({comment: this.state.text, parent_id: this.props.parentId, name: CURRRENT_NAME}),
+      headers: { 'content-type': 'application/x-www-form-urlencoded' },
+
+    });
+
+    this.setState({text: "" });
+
+    if (this.props.onSubmit)
+      this.props.onSubmit();
+  }
+
+  onChange(event) {
+    this.setState({ text: event.target.value });
+  }
+
+  render() {
+    return (
+        <div class="one-line section-border">
+          <input onChange={this.onChange} id="comment-line" type="text" class="form-control" placeholder="What are your thoughts?" value={this.state.text} />
+          <button onClick={this.onSubmit} type="submit" class="btn btn-default bg-primary reply">Submit</button>
+        </div>
+    );
+  }
+}
+
+class Comments extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {comments: [] };
+  }
+
+  refresh() {
+    fetch('/api/comments')
+          .then(response => response.json())
+          .then(data => {
+
+        let seen = {};
+        data.forEach(parent => {
+          data.forEach(child => {
+            if (child.parent_id != parent.comment_id)
+              return;
+
+            if (!parent.children)
+              parent.children = [];
+
+            parent.children.push(child);
+            seen[child.comment_id] = true;
+          });
+
+          parent.timestamp = dayjs.utc(parent.timestamp);
+        });
+
+        data = data.filter(c => !seen[c.comment_id]);
+
+        let order = comments => {
+          comments.sort((a, b) => {
+            return b.timestamp - a.timestamp
+          })
+
+          comments.forEach(c => {
+            if (c.children)
+              order(c.children);
+          })
+        };
+        order(data);
+
+        this.setState({ comments: data.filter(c => !seen[c.comment_id]) })
+    });
+  }
+
+  componentDidMount() {
+      this.refresh();
+
+      this.socket = new WebSocket('ws://' + location.host);
+
+      let self = this;
+      this.socket.onmessage = function(event) {
+        self.refresh();
+      };
+  }
+
+  componentWillUnmount() {
+    if (this.socket)
+      this.socket.close();
+  }
+
+  render() {
+    return ( 
+      <div>
+        <CommentBox />
+        <div id="comments">
+        { 
+          this.state.comments.map((c) => 
+            <Comment key={c.comment_id} id={c.comment_id} timestamp={c.timestamp} name={c.name} comment={c.comment} upvotes={c.upvotes} children={c.children} /> 
+          ) 
+        } 
+        </div>
+      </div>
+    );
+  }
+}
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return (
+      {}
+    );
+  }
+}
+
+const element = <Comments />
+const domContainer = document.querySelector('#comments_container');
+const root = ReactDOM.createRoot(domContainer);
+root.render(element);
