ce70f88dbf1c10674029b6a8d9221c6181dfc3ad
unknown <andrew.joe.jones@gmail.com>
2022-04-20 23:51:17 -0700

    Full functionality sans database and input validation

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
 
+const uuid  = require('uuid');
+
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -13,7 +15,18 @@ app.get('/api/comments', (req, res) => {
 })
 
 app.post('/api/comment', (req, res) => {
-  comments.push(req.body);
+  let comment = req.body;
+  comment.upvotes = 0;
+  comment.id = uuid.v4();
+  comments.push(comment);
+  res.send("good job!");
+})
+
+app.post('/api/upvote', (req, res) => {
+  comments.forEach((c) => {
+    if (c.id === req.body.comment_id)
+      c.upvotes++;
+  })
   res.send("good job!");
 })
 
