aebd770342803de298ad8e3c8907f2a3fd61aa1e
unknown <andrew.joe.jones@gmail.com>
2022-04-21 11:47:30 -0700

    port 80

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const ws = require('ws');
 const bodyParser = require('body-parser');
 const app = express()
-const port = 3000
+const port = 80
 
 const uuid  = require('uuid');
 
